d963c944640d053f3a1f586b344b99b0d2c1e6ab
zaynaib <zaynaibg@gmail.com>
2023-07-16 19:51:53 -0500

    opacity is not working

--- copy_learn/ukraine_viz/eda_ukraine.R
+++ copy_learn/ukraine_viz/eda_ukraine.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-
+library(scales)
+library(ggtext)
 ukraine_raw <- read_csv('data/results.csv')
 colnames(ukraine_raw)
 head(ukraine_raw)
@@ -13,6 +14,8 @@ ukraine_finacial_aid <- ukraine_raw %>%
   group_by(`Fiscal Year`) %>%
   summarise(sum_aid_dollars = sum(`Current Dollar Amount`))
 
+ukraine_finacial_aid
+
 #stat identity allows you to plot bars that are the length of the mappints
 #that I specify. 
 ukraine_finacial_aid %>%
@@ -21,21 +24,60 @@ ggplot(aes(x=`Fiscal Year`,y=sum_aid_dollars)) +
 
 
 ukraine_finacial_aid %>%
-  filter(`Fiscal Year` >= 2000) %>%
+  filter(`Fiscal Year` >= 2000 & `Fiscal Year` < 2023) %>%
   ggplot(aes(x=`Fiscal Year`,y=sum_aid_dollars)) +
   geom_bar(stat='identity')
 
+
+
+#disable scientific notation 
+#options is a global variable
+#https://pubs.wsb.wisc.edu/academics/analytics-using-r-2019/s-options.html
+options(scipen = 999)
+
 #change the theme
+
+#notes for labels : https://rpubs.com/techanswers88/remove-scientific-notation-in-ggplot
+#note** label_number_si is decapitated new doc in this blog post:https://www.tidyverse.org/blog/2022/04/scales-1-2-0/
+#expand gets rid of padding between the axis and the panel
+#note text https://wilkelab.org/ggtext/
+
+#add line breaks to text udemy section 8 video 100
+
 ukraine_finacial_aid %>%
-  filter(`Fiscal Year` >= 2000) %>%
-  ggplot(aes(x=`Fiscal Year`,y=sum_aid_dollars)) +
+  filter(`Fiscal Year` >= 2000  & `Fiscal Year` < 2023) %>%
+  ggplot(aes(x=`Fiscal Year`,y=sum_aid_dollars,aph)) +
   geom_bar(stat='identity',fill = "#C88967") +
-  scale_y_continuous(position = "right")+
+  scale_y_continuous(position = "right",labels = label_number(scale_cut = cut_short_scale()),
+                     breaks=seq(0,14000000000,2000000000),
+                     expand = expansion(0))+
+  scale_x_discrete(expand = expansion(0.005)) +
+  labs(title = 'The Brief',
+       subtitle='**U.S Foreign Aid to Ukraine** <br>The $12 billion in traditional foreign aid Congrees allocated for Ukraine last week in 10<br> times more than the assistance fhe country was given in the 2020 fiscal year.',
+       caption = 'By The New York Times <br><br>  Source: ForeginAssistance.gov The new bill may include funding not covered in the historical data,<br> which is adjusted to 2020 dollars. Data not available for fiscal year 2021') +
   theme(panel.background = element_blank(),
         axis.line.x = element_line(color = "black"),
-     
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        plot.title = element_text(face = "bold"),
+        plot.caption = element_markdown(),
+        plot.subtitle =   element_markdown(lineheight = 1.2)
         
-        )
+  ) 
 
+#https://stackoverflow.com/questions/28979622/ggplot-transparency-on-individual-bar
+ukraine_finacial_aid_alpha = ukraine_finacial_aid
+
+ukraine_finacial_aid_alpha$alphayear = "a"
+
+ukraine_finacial_aid_alpha[33,1:2]
+ukraine_finacial_aid_alpha[33,3] <- "b"
+
+
+ukraine_finacial_aid_alpha %>%
+  filter(`Fiscal Year` >= 2000  & `Fiscal Year` < 2023) %>%
+  ggplot(aes(x=`Fiscal Year`,y=sum_aid_dollars))+
+  geom_bar(stat='identity',fill = "#C88967",alpha=factor(alphayear)) 
 
 #plot.background = element_rect(fill = "#EC4899", color = NA)
